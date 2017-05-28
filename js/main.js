54af0f06615517e315b261b4fa531db2251d9530
Allan S <allansp9@gmail.com>
2017-05-28 19:57:41 +0000

    pagina de detalhes v1

--- js/main.js
+++ js/main.js
@@ -1,77 +1,82 @@
 $(document).ready(function(){
-    $('#searchText').on('keyup', function(){
-        var searchText = $('#searchText').val(); 
-        buscaFilmes(searchText);
+    $('#searchForm').on('keyup', (e) => {
+        let searchText = $('#searchText').val();
+        getFilmes(searchText);
+        e.preventDefault();
     });
-});    
+});
+
+function getFilmes(searchText){
+    let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.themoviedb.org/3/search/movie?api_key=2657b90452d2f9814a444d1074c32cab&language=en-US&query="+searchText+"&page=1&include_adult=false",
+        "method": "GET",
+        "headers": {},
+        "data": "{}"
+    };
+
+    $.ajax(settings).done(function (response) {
+        let movies = response.results;
+        let output = '';
+        $.each(movies, (index, movie) => {
+            output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="https://image.tmdb.org/t/p/w154/${movie.poster_path}">
+                        <h5>${movie.title}</h5>
+                        <a onclick="getFilmeId('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
+                    </div>
+                </div>
+            `;
+        });
+        $('#movies').html(output);
+   
+    });
+}
+
+  
+function getFilmeId(id){
+    sessionStorage.setItem('movieId', id);
+    window.location = '../views/detalhes.php';
+    return false;
+}
+
+function getDetalhes(){
+    let movieId = sessionStorage.getItem('movieId');
     
-    function buscaFilmes(texto){
-        $.getJSON(
-            "https://api.themoviedb.org/3/search/movie?api_key=2657b90452d2f9814a444d1074c32cab&language=en-US&query="+texto+"&page=1",
-            function(response){
-                let movies = response.results;
-                let output = '';
-                $.each(movies, (index, movie) => {
-                    output += `
-                        <div class="col-md-3">
-                            <div class="well text-center">
-                                <img src="https://image.tmdb.org/t/p/w154/${movie.poster_path}">
-                                <h5>${movie.title}</h5>
-                                <a onclick="getFilmeId('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
-                            </div>
-                        </div>
-                    `;
-                });
-                $('#movies').html(output);
-            }
+    let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.themoviedb.org/3/movie/"+movieId+"?api_key=2657b90452d2f9814a444d1074c32cab&language=pt-BR",
+        "method": "GET",
+        "headers": {},
+        "data": "{}"
+    };
+
+    $.ajax(settings).done(function (response) {
+        let movie = response;
+        let output =`
+        <div class="row">
+          <div class="col-md-4">
+            <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">
+          </div>
+          <div class="col-md-8">
+            <h2>${movie.title}</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Plot</h3>
+            ${movie.overview}
+            <hr>
+            <a href="../views/index.php" class="btn btn-default">Voltar</a>
+          </div>
+        </div>
+      `;
         
-        )
-    }
-    
-    function getFilmeId(id){
-        sessionStorage.setItem('movieId', id);
-        window.location = '../views/detalhes.php';
-        return false;
-    }
-    
-    function getDetalhes(){
-        var movieId = sessionStorage.getItem('movieId');
-        $.getJSON(
-            "https://api.themoviedb.org/3/movie/"+movieId+"?api_key=2657b90452d2f9814a444d1074c32cab&language=pt-BR",
-            function(response){
-                let output = '';
-                $.each(response, (index, movie) => {
-                    console.log(movie);
-                    output =`
-                            <div class="row">
-                              <div class="col-md-4">
-                                <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">
-                              </div>
-                              <div class="col-md-8">
-                                <h2>${movie.title}</h2>
-                                <ul class="list-group">
-                                  <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-                                  <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-                                  <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-                                  <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-                                  <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-                                  <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-                                  <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
-                                </ul>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="well">
-                                <h3>Plot</h3>
-                                ${movie.Plot}
-                                <hr>
-                                <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-                                <a href="index.html" class="btn btn-default">Go Back To Search</a>
-                              </div>
-                            </div>
-                          `;
-                });
-                $('#movie').html(output);
-            }
-        );
-    }
+        $('#movie').html(output);
+
+   
+    });
+}
